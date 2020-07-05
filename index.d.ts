bc0d631e29ed17a7f352d01d68e01228dca593e0
Xetera <mythologyali@gmail.com>
2020-07-05 14:24:20 -0700

    Apply suggestions from code review
    Co-authored-by: lleyton <lleyton@innatical.com>

--- index.d.ts
+++ index.d.ts
@@ -9,12 +9,12 @@ declare module "discord-webook-node" {
     setUsername(username: string): void;
     setAvatar(avatar: string): void;
     sendFile(filePath: string): Promise<void>;
-    send(message: MessageBuilder): Promise<void>;
+    send(message: MessageBuilder | string): Promise<void>;
     info(
       title: string,
       fieldName: string,
       fieldValue: string,
-      inline: boolean
+      inline?: boolean
     ): void;
     warning(
       title: string,
@@ -30,20 +30,20 @@ declare module "discord-webook-node" {
     ): void;
   }
   export type WebhookField = {
-    name?: string;
-    value?: string;
+    name: string;
+    value: string;
     inline?: boolean;
   };
   export type WebhookEmbed = {
     author?: {
-      name: string;
-      url: string;
-      icon_url: string;
+      name?: string;
+      url?: string;
+      icon_url?: string;
     };
     title?: string;
     url?: string;
     thumbnail?: {
-      url: string;
+      url?: string;
     };
     image?: {
       url: string;
@@ -53,7 +53,7 @@ declare module "discord-webook-node" {
     description?: string;
     fields: WebhookField[];
     footer?: {
-      text?: string;
+      text: string;
       icon_url?: string;
     };
   };
@@ -64,15 +64,16 @@ declare module "discord-webook-node" {
 
   export class MessageBuilder {
     constructor();
-    getJson(): WebhookPayload;
+    getJSON(): WebhookPayload;
     setText(text: string): this;
-    setAuthor(author: string, authorImage?: string, authorUrl?: string): this;
+    setAuthor(author?: string, authorImage?: string, authorUrl?: string): this;
     setTitle(title: string): this;
     setUrl(url: string): this;
     setThumbnail(thumbnailUrl: string): this;
+    setImage(image: string): this;
     setTimestamp(): this;
     setColor(color: number): this;
-    setDescription(description: number): this;
+    setDescription(description: string): this;
     addField(fieldName: string, fieldValue: string, inline?: boolean): this;
     setFooter(footer: string, footerImage?: string): this;
   }
