34159980e9ec0554ba347d78f521962c20b69554
Carbon Alabel <carbonalabel@gmail.com>
2020-07-18 02:30:58 +0200

    Support ESM

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const Webhook = require('./classes/webhook');
 const MessageBuilder = require('./classes/messageBuilder');
 
 module.exports = {
-    default: {
-        Webhook,
-        MessageBuilder
-    },
     Webhook,
     MessageBuilder
 };
